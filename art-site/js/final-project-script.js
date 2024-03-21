4e6f46591c612326b538563981721436db28b834
Riley Brunn <brunn.riley323@gmail.com>
2024-03-20 22:01:19 -0400

    Delete art-site directory

--- art-site/js/final-project-script.js
+++
@@ -1,7 +0,0 @@
-function generateGreeting() {
-    let input = document.querySelector('#user-name');
-    let userInput = input.value;
-    alert('Hello ' + userInput + '!');
-}
-
-let submit = document.querySelector('#submit').addEventListener('click', generateGreeting);
